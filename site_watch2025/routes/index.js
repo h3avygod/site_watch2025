4876345a37abbb23521ab7c18076e566f5a771b3
h3avygodя <_email@example.com>
2025-12-10 19:40:36 +0300

    tc_4_3

--- site_watch2025/routes/index.js
+++ site_watch2025/routes/index.js
@@ -3,13 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/casio', function(req, res, next) {
-    res.send("<h1>Page Casio</h1>")
+    res.render('watch', {
+        title: "Page Casio",
+        picture: "images/casio.png",
+        desc: "Casio — легенда на запястье Японская надёжность, культовый дизайн и технологии, проверенные поколениями."
+    });
 });
 router.get('/philipe', function(req, res, next) {
-    res.send("<h1>Page Philipe Patek</h1>")
+    res.render('watch', {
+        title: "Page Philipe Patek",
+        picture: "images/philipe.png",
+        desc: "Patek Philippe — тихая вселенная вечности.Не просто часы, а фамильные драгоценности, которые переходят от отца к сыну. Швейцарское совершенство, которое не торопится."
+    });
 });
 router.get('/rolex', function(req, res, next) {
-    res.send("<h1>Page Rolex</h1>")
+    res.render('watch', {
+        title: "Page Rolex",
+        picture: "images/rolex.png",
+        desc: "Rolex — не просто часы, а символ.Узнаваемый, весомый, уверенный. Марка, которая превратила инструмент в икон"
+    });
 });
 
 module.exports = router;
