e587588c388f048a775c3af4e2dedfb737d2214e
h3avygodя <_email@example.com>
2025-12-10 19:20:34 +0300

    watch_4_2

--- site_watch2025/routes/index.js
+++ site_watch2025/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/casio', function(req, res, next) {
+    res.send("<h1>Page Casio</h1>")
+});
+router.get('/philipe', function(req, res, next) {
+    res.send("<h1>Page Philipe Patek</h1>")
+});
+router.get('/rolex', function(req, res, next) {
+    res.send("<h1>Page Rolex</h1>")
 });
 
 module.exports = router;
